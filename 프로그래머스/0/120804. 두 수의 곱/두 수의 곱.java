82d57ea008988b80cc284ae7caab8827774f57e0
이재윤 <kjd99002@naver.com>
2024-05-30 23:10:18 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer=test(num1,num2);
+        return answer;
+    }
+    public int test(int a,int b){
+        int t=0;
+        t=a*b;
+        return t;
+    }
+}
