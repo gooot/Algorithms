cd67b251e442090d310e845b13e32ef290201e05
이재윤 <kjd99002@naver.com>
2024-05-31 00:29:04 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int sum=0;
+        
+        for(int i=0;i<=n;i=i+2){
+            sum=sum+i;
+}
+        answer=sum;
+        
+        
+        return answer;
+    }
+}
