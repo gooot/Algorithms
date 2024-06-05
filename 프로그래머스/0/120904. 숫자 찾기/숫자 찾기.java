8ffcdfa74ba0f833804647c0ebb3cbb515cd36e4
이재윤 <kjd99002@naver.com>
2024-06-06 02:08:07 +0900

    [level 0] Title: 숫자 찾기, Time: 0.16 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        
+        String s = Integer.toString(num);
+        String[] arr = s.split("");
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals(Integer.toString(k))){
+                return i+1;
+            }
+        }
+        
+        return -1;
+        
+        
+        
+    }
+}
