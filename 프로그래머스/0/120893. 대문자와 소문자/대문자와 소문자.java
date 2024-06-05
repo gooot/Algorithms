1ac9cb44d999946e2623f9c474ec4209f560929c
이재윤 <kjd99002@naver.com>
2024-06-05 15:56:56 +0900

    [level 0] Title: 대문자와 소문자, Time: 18.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        
+        for(int i =0;i<my_string.length();i++){
+            if(Character.isUpperCase(my_string.charAt(i)))
+                answer= answer+Character.toLowerCase(my_string.charAt(i));
+            else
+                answer= answer+Character.toUpperCase(my_string.charAt(i));
+        }
+        
+        
+        
+        return answer;
+    }
+}
