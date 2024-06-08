2a8f1f23ef13f82ae2e814299d90b2ca20b2657f
이재윤 <kjd99002@naver.com>
2024-06-09 02:04:51 +0900

    [level 0] Title: 문자열 뒤집기, Time: 16.90 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        
+        sb=new StringBuilder(my_string.substring(s,e+1));
+        sb.reverse();
+        
+        answer=my_string.substring(0,s) + sb + my_string.substring(e+1);
+        
+            
+        return answer;
+    }
+}
