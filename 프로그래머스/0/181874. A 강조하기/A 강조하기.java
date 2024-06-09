e7e1f221ac8fedbedb3e522320bb65579dd49926
이재윤 <kjd99002@naver.com>
2024-06-10 02:06:17 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        myString=myString.toLowerCase().replace('a','A');
+        answer=myString;
+        return answer;
+    }
+}
