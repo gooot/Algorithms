b76a5c833835c2af5054669a9cb273f46d0f9127
이재윤 <kjd99002@naver.com>
2024-06-10 21:30:58 +0900

    [level 0] Title: x 사이의 개수, Time: 3.15 ms, Memory: 97 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(String myString) {
+        int[] answer = {};
+        String[] str ={};
+        
+        str = myString.split("x",-1);
+        answer=new int[str.length];
+        
+        for(int i=0;i<str.length;i++){
+            answer[i]=str[i].length();
+        }
+        
+        return answer;
+    }
+}
