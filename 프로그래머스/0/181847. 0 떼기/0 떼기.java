65a45b7bf7ec0414d1810cea14dbbb308633bb6d
이재윤 <kjd99002@naver.com>
2024-06-13 23:29:24 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int a=0;
+        a= Integer.parseInt(n_str);
+        
+        answer=String.valueOf(a);
+        
+        return answer;
+    }
+}
