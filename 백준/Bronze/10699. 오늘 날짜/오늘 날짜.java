c6f94fedbefc939983052cbe8e3b37a89c9c4bcb
이재윤 <kjd99002@naver.com>
2024-06-20 19:25:41 +0900

    [Bronze V] Title: 오늘 날짜, Time: 108 ms, Memory: 13116 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,26 @@
+
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		LocalDate date = LocalDate.now();
+		
+		System.out.println(date);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
