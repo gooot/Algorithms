206adabbc9ee0f0f9fd20b05d1517ccc6f2765b8
이재윤 <kjd99002@naver.com>
2024-06-22 05:17:13 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 12808 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s1 = new Scanner(System.in);
+		String str = s1.nextLine();
+		
+		
+		System.out.println(str+"??!");
+	}
+
+}
