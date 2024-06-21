d3b05da87034620cffe9bf37bf3ce871ac25cda2
이재윤 <kjd99002@naver.com>
2024-06-22 05:52:12 +0900

    [Bronze V] Title: A/B, Time: 116 ms, Memory: 16000 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.concurrent.SynchronousQueue;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double A=0.0;
+		double B=0.0;
+		
+		Scanner Input = new Scanner(System.in);
+		
+		
+		A=Input.nextDouble();
+		B=Input.nextDouble();
+		
+		System.out.println(A/B);
+		
+		
+		
+	}
+
+}
