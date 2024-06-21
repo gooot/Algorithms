0197725b5d1ffb0eaf26362ca5fd2cd9a8f90620
이재윤 <kjd99002@naver.com>
2024-06-22 05:15:20 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 112 ms, Memory: 12884 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s1 = new Scanner(System.in);
+		int year = s1.nextInt();
+		
+		System.out.println(year-543);
+	}
+
+}
