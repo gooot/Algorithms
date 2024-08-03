62c90f4d61982f01e46d3c476300761c7dedc3d3
이재윤 <kjd99002@naver.com>
2024-08-04 01:20:17 +0900

    [Silver V] Title: 거스름돈, Time: 72 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		int price = 0;
+		int change = 0;
+
+		Map<String, Integer> map = new HashMap<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		price = Integer.parseInt(br.readLine());
+
+		change = setUp(map, price, change);
+
+
+
+		while (map.get("5") >= 0) {
+
+			if(change==0){
+				System.out.println(map.get("5"));
+				break;
+			}
+
+
+			if (countChange(map, change)) {
+				System.out.println(Math.addExact(map.get("5").intValue(),map.get("2").intValue()));
+				break;
+			} else {
+				map.put("5",map.get("5")-1);
+				change+=5;
+			}
+
+		}
+		if(map.get("5")==-1)
+			System.out.println("-1");
+
+
+	}
+
+	public static int setUp(Map<String, Integer> map, int price,int change) {
+		map.put("5", 0);
+		map.put("2", 0);
+		map.put("5", price / 5);
+		change = price - Integer.parseInt(String.valueOf(map.get("5"))) * 5;
+		return price - ((map.get("5")) * 5);
+
+	}
+
+	public static boolean countChange(Map<String, Integer> map, int change) {
+		if (change % 2 == 0) {
+			map.put("2", change / 2);
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
