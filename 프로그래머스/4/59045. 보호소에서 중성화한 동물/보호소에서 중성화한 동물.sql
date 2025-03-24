d2e8fd0c7e715a6d14eacf954ce3e722b06ca778
이재윤 <kjd99002@naver.com>
2025-03-24 20:39:04 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+select ai.animal_id , ai.animal_type , ai.name 
+from animal_ins ai
+join animal_outs ao on ai.animal_id = ao.animal_id
+where ai.sex_upon_intake like 'intact%' 
+and (ao.sex_upon_outcome like 'Spayed%'
+or ao.sex_upon_outcome like 'Neutered%')
+order by ai.animal_id
